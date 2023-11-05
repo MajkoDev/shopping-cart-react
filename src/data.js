81c8183b9e9a7e49264df8bb27eb524b14995177
Marian <hellomariandev@gmail.com>
2023-11-05 12:48:29 +0100

    mock data

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const storeData = [
+  {
+    name: "London",
+    price: 125.35,
+  },
+  {
+    name: "Berlin",
+    price: 75.95,
+  },
+  {
+    name: "Paris",
+    price: 185.65,
+  },
+  {
+    name: "Rome",
+    price: 105.05,
+  },
+  {
+    name: "Moscow",
+    price: 85.15,
+  },
+];
