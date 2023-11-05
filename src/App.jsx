6bb85c12b68f71235483b6ef0a00ffb0e16e98e3
Marian <hellomariandev@gmail.com>
2023-11-05 12:48:36 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Basket from "./pages/Basket";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <h1 className="text-center text-lg font-semibold">
+      <h1 className="text-center text-lg font-semibold py-3 bg-slate-100">
         Shopping Cart with React Hooks and Context
       </h1>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/basket" element={<Basket />} />
@@ -19,3 +21,4 @@ function App() {
 }
 
 export default App;
+ 
