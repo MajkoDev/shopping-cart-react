7c533967ce2e0b569dd9952d8c7b37e5b53c745a
Marian <hellomariandev@gmail.com>
2023-11-05 12:34:29 +0100

    react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Basket from "./pages/Basket";
+
 function App() {
   return (
     <>
-      <h1>Shopping Cart with React Hooks and Context</h1>
-
-      
+      <h1 className="text-center text-lg font-semibold">
+        Shopping Cart with React Hooks and Context
+      </h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/basket" element={<Basket />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
