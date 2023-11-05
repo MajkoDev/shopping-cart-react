01034325de3ce533228c4a69ddadd1e75cece1dc
Marian <hellomariandev@gmail.com>
2023-11-05 12:48:34 +0100

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { storeData } from "../data";
+import Product from "../components/Product";
 
 const Home = () => {
+  console.log(storeData);
   return (
-    <div>Home</div>
-  )
-}
+    <div className="flex flex-col items-center">
+      {storeData.map((item, i) => (
+        <Product key={i} item={item} />
+      ))}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
